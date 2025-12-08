cb6e66f42740f31a4e31d393e383c55b8bfc4cfe
manohar0548 <manoharlakkaraju@gmail.com>
2025-12-08 14:53:50 +0530

    New Changes

--- tests/HomePage.spec.js
+++ tests/HomePage.spec.js
@@ -5,5 +5,8 @@ import { test, expect } from '@playwright/test';
 
 test('Home Page', async ({ page }) => {
   await page.goto('https://demoblaze.com/');
+  const pageTitle = page.title();
+  console.log("Page Title is: " + await pageTitle);
+    await expect(pageTitle).resolves.toMatch('STORE');
 }
 )
